a14f96e51bcaca1234440569235b346c91b79d97
Sofía Silva Berenguer <sofia@apnic.net>
2018-06-23 13:43:16 +0100

    Adding code to download and process json file from AS Hegemony API

--- AShegemony_postProc.py
+++ AShegemony_postProc.py
@@ -6,8 +6,22 @@ Created on Sat Jun 23 11:32:02 2018
 @author: sofia
 """
 import requests
+from datetime import date
 
+today = date.today()
 
-hege_url = 'https://ihr.iijlab.net/ihr/api/hegemony/?originasn=2914&timebin__gte=2018-06-19+08:00&af=4'
+originAS = 2914
+
+hege_url = 'https://ihr.iijlab.net/ihr/api/hegemony/?originasn={}&timebin={}+08:00&af=4'.format(originAS,
+                                                                                                today.strftime('%Y-%m-%d'))
 
 hege_json = requests.get(hege_url).json()
+
+nodes_dict = {}
+
+for res in hege_json['results']:
+    if res['asn'] not in nodes_dict:
+        nodes_dict[res['asn']] = {}
+    
+    nodes_dict[res['asn']]['hege'] = res['hege']
+    nodes_dict[res['asn']]['asn_name'] = res['asn_name']
